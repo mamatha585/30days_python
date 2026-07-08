73231389273069a3fa0dc5d19864e1d62988aa04
mamatha585 <mamathaavudoddi875@gmail.com>
2026-07-08 10:03:05 +0530

    Exercise2

---
+++ variable.py
@@ -0,0 +1,26 @@
+# 30 days of python programming
+#level 1
+first_name = 'Mamatha'
+last_name = 'Avudoddi'
+full_name = first_name + last_name
+country = 'India'
+city = 'Hyderabad'
+age = 22
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+a,b,c = 2,3,4
+
+# level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a))
