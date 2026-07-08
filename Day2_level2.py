ff610c3cd84671efff58c944048472b83604db4f
Mamatha <mamathaavudoddi875@gmail.com>
2026-07-08 06:07:06 +0000

    Day2:level2

---
+++ Day2_level2.py
@@ -0,0 +1,60 @@
+# level 2
+first_name = 'Mamatha'
+last_name = 'Avudoddi'
+full_name = first_name +' ' + last_name
+country = 'India'
+city = 'Hyderabad'
+age = 22
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+a,b,c = 2,3,4
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(a),type(b) ,type(c))
+
+print(len(first_name))
+print(len(first_name) > len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+quotient = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+area_of_circle = 3.14* radius**2
+circumference = 2*3.14*radius
+rad = int(input("Enter the radius of a circle: "))
+area = 3.14*rad**2
+firstname = input("Enter your first name: ")
+lastname = input("Enter your last name:")
+country = input("Enter your country: ")
+age = int(input("Enter your age: "))
+
+print("Total is: ", total)
+print("Difference is: ", diff)
+print("Product is: ", product)
+print("Quotient is: ", quotient)
+print("Remainder is: ", remainder)
+print("Exponent is: ", exp)
+print("Floor Division is: ", floor_division)
+print("Area of Circle is: ", area_of_circle)
+print("Circumference of Circle is: ", circumference)
+print("Area of Circle with user input is: ", area)
+print("First Name is: ", firstname)
+print("Last Name is: ", lastname)
+print("Country is: ", country)
+print("Age is: ", age)
